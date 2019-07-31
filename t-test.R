aa1683ca2aa4d9e707444139e05cd67b1f78df9a
apopetic <48439275+apopetic@users.noreply.github.com>
2019-07-31 13:34:35 +0530

    Apopoetic
    Run t-test using means, std dev, and sample size

---
+++ t-test.R
@@ -0,0 +1,22 @@
+#this function calculates t-statistic for two independent samples using mean, SD, sample size, and level of significance *******
+#default value of significance is set at 0.05 ********
+#m1, sd1, n1 are mean, std dev. and sample size of 1st treatment. m2, sd2, n2 are mean, std dev. and sample size of 1st treatment *****
+#p = level of significance, which can be changed while running the function ******
+#tl = two tailed or one tailed; default set to 2-tailed and can be changed to 1. ******
+#This script was made by Ankur Jamwal on 31 July 2019***************
+
+t_test <- function(m1, m2, sd1, sd2, n1, n2, p = 0.05, tl = 2){
+  t <- (m2-m1)/sqrt((sd2^2/n2)+(sd1^2/n1))
+  df <- ((n1+n2-2))
+  pt <- tl*pt(t,df)
+  if(pt < p){
+    paste0("there is significant difference between treatment at ", p, " level", " the t-statistic is ", t, " the p-value is ", pt, " and has ", df, " degrees of freedom")
+  }
+else {
+  paste("treatments are not statistically significant at", p, "level.", "The t-statistics is ", t, " the p-value is ", pt)
+}
+}
+
+t_test(1, 0.714, 0.0714, 0.0341, 4, 4, tl = 1) #input the value and just run it!!
+
+
